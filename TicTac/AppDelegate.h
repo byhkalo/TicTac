17ab8e39a79ab340a4824cca2a7439d35bf8c91c
Konstantyn Bykhkalo <konstantyn_bykhkalo@Mac-mini-Konstantyn.local>
2014-12-23 00:34:07 +0200

    Initial Commit

---
+++ TicTac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTac
+//
+//  Created by Konstantyn Bykhkalo on 23.12.14.
+//  Copyright (c) 2014 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTac
+//
+//  Created by Konstantyn Bykhkalo on 23.12.14.
+//  Copyright (c) 2014 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
